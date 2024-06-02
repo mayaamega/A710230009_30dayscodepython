d9f9fa6a6cb88c0183f406b137fd669ca54694e6
mayaamega <a710230009@student.ums.ac.id>
2024-06-02 14:33:55 +0700

    first commit

---
+++ day1.py
@@ -0,0 +1,11 @@
+# Variabel dan tipe data
+nama = "John"
+umur = 25
+tinggi = 175.5
+menikah = False
+
+# Mencetak nilai variabel
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Menikah?", menikah)
