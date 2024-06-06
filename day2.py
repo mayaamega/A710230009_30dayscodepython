a1dbf38996de454724b02078f3ac85493b9159d9
mayaamega <a710230009@student.ums.ac.id>
2024-06-06 17:57:10 +0700

    day6.py

--- day2.py
+++ day2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def sort(lst):
     for j in range(len(lst) - 1):
         for i in range(len(lst) - 1 - j):
@@ -11,17 +10,3 @@ list = [30, 23, 40, 11, 3]
 print('Angka yang akan diurutkan:', list)
 print('Proses sorting:')
 sort(list)
-=======
-def sort(lst):
-    for j in range(len(lst) - 1):
-        for i in range(len(lst) - 1 - j):
-            if lst[i] > lst[i + 1]:
-                lst[i], lst[i + 1] = lst[i + 1], lst[i]
-        print(">> Iterasi:", j, "List:", lst)
-
-list = [30, 23, 40, 11, 3]
-
-print('Angka yang akan diurutkan:', list)
-print('Proses sorting:')
-sort(list)
->>>>>>> 10fc855b0f628f2a220251b03c5dc2c691fb209c
